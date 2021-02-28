2912a246b519bc52dc9966ae59e71599ad9848b2
Andrew Osheroff <andrewosh@gmail.com>
2021-03-01 00:55:48 +0100

    Added working ImmutableStore

---
+++ immutable-store/index.js
@@ -0,0 +1,187 @@
+const sodium = require('sodium-universal')
+
+const { Message } = require('./lib/messages')
+
+const VERSION = 'v1'
+const CAP_NAMESPACE = Buffer.from(`immutable store capability ${VERSION}`)
+const EXTENSION_NAME = `immutable-store/${VERSION}`
+// How long to wait for a reply.
+const DEFAULT_TIMEOUT = 2.5 * 1000
+
+class MapDb {
+  constructor () {
+    this.m = new Map()
+  }
+
+  put (key, val) {
+    if (Buffer.isBuffer(key)) key = key.toString('hex')
+    this.m.set(key, val)
+  }
+
+  get (key) {
+    if (Buffer.isBuffer(key)) key = key.toString('hex')
+    return this.m.get(key)
+  }
+}
+
+class InvertedPromise {
+  constructor (resolve, reject) {
+    this.resolve = resolve
+    this.reject = reject
+  }
+}
+
+module.exports = class ImmutableStore {
+  constructor (opts = {}) {
+    this.db = opts.db || new MapDb()
+
+    this._debug = opts.debug
+    this._onerror = opts.onerror
+    this._timeout = opts.timeout || DEFAULT_TIMEOUT
+    this._exts = new Map()
+    this._pending = new Map()
+  }
+
+  _verifyCapability (stream, hash, cap) {
+    const expected = deriveCapability(hash, stream.remotePublicKey)
+    return sodium.sodium_compare(expected, cap) === 0
+  }
+
+  _send (ext, msg) {
+    const state = { start: 0, end: 0, buffer: null }
+    Message.preencode(state, msg)
+    state.buffer = Buffer.alloc(state.end)
+    Message.encode(state, msg)
+    return ext.send(state.buffer)
+  }
+
+  _sendRequests (keys, pending) {
+    for (const [stream, ext] of this._exts) {
+      const request = {
+        discoveryKey: keys.discoveryKey,
+        capability: deriveCapability(keys.hash, stream.publicKey)
+      }
+      this._send(ext, { request })
+      pending.inflight.add(stream)
+    }
+    pending.timeout = setTimeout(() => this._flush(keys.id, null), this._timeout)
+  }
+
+  _flush (id, value) {
+    if (!this._pending.has(id)) return
+    const { promises, timeout } = this._pending.get(id)
+    clearTimeout(timeout)
+    for (const prom of promises) {
+      prom.resolve(value)
+    }
+    this._pending.delete(id)
+  }
+
+  // Extension Handlers
+
+  async _onrequest (req, from) {
+    const discoveryKey = req.discoveryKey
+    const ext = this._exts.get(from)
+    if (!discoveryKey || !ext) return
+
+    const rsp = { response: { discoveryKey } }
+    const stored = await this.db.get(discoveryKey)
+    const verified = stored && this._verifyCapability(from, stored.hash, req.capability)
+
+    if (!stored || !verified) return this._send(ext, rsp)
+
+    rsp.response.hash = stored.hash
+    rsp.response.value = stored.value
+    this._send(ext, rsp)
+  }
+
+  async _onresponse (rsp, from) {
+    const pending = this._pending.get(keyString(rsp.discoveryKey))
+    if (!pending || !pending.inflight.has(from)) return
+
+    pending.inflight.delete(from)
+    if (!rsp.hash) return
+
+    const valueHash = deriveHash(rsp.value)
+    const discoveryKey = deriveHash(rsp.hash)
+
+    if (valueHash.equals(rsp.hash) && discoveryKey.equals(rsp.discoveryKey)) {
+      await this.db.put(rsp.discoveryKey, { hash: rsp.hash, value: rsp.value })
+      this._flush(keyString(discoveryKey), rsp.value)
+    }
+  }
+
+  async _onmessage (raw, from) {
+    try {
+      const state = { start: 0, end: raw.length, buffer: raw }
+      const msg = Message.decode(state)
+      if (this._debug && this._debug.onmessage) this._debug.onmessage(msg, from)
+      if (msg.request) await this._onrequest(msg.request, from)
+      else if (msg.response) await this._onresponse(msg.response, from)
+    } catch (err) {
+      if (this._onerror) this._onerror(err)
+    }
+  }
+
+  // Public API
+
+  async put (value) {
+    const hash = deriveHash(value)
+    const discoveryKey = deriveHash(hash)
+    await this.db.put(discoveryKey, { hash, value })
+    return hash
+  }
+
+  async get (hash, opts = {}) {
+    const discoveryKey = deriveHash(hash)
+    const existing = await this.db.get(discoveryKey)
+    if (existing || opts.local || !this._exts.size) return existing.value
+
+    const id = keyString(discoveryKey)
+    const keys = { hash, discoveryKey, id }
+
+    let pending = this._pending.get(id)
+    if (!pending) {
+      pending = { inflight: new Set(), promises: [], timeout: null }
+      this._pending.set(id, pending)
+      this._sendRequests(keys, pending)
+    }
+
+    return new Promise((resolve, reject) => {
+      pending.promises.push(new InvertedPromise(resolve, reject))
+    })
+  }
+
+  register (stream) {
+    const ext = stream.registerExtension(EXTENSION_NAME, {
+      onmessage: (msg) => this._onmessage(msg, stream)
+    })
+    this._exts.set(stream, ext)
+  }
+
+  close () {
+    for (const ext of this._exts.values) {
+      ext.destroy()
+    }
+    if (this.db.close) return this.db.close()
+  }
+}
+
+function keyString (key) {
+  return Buffer.isBuffer(key) ? key.toString('hex') : key
+}
+
+function deriveCapability (key, noisePublicKey) {
+  const out = Buffer.allocUnsafe(32)
+  sodium.crypto_generichash_batch(out, [
+    CAP_NAMESPACE,
+    noisePublicKey
+  ], key)
+  return out
+}
+
+function deriveHash (buf) {
+  const digest = Buffer.allocUnsafe(32)
+  sodium.crypto_generichash(digest, buf)
+  return digest
+}
---
+++ immutable-store/test/all.js
@@ -0,0 +1,141 @@
+const sodium = require('sodium-universal')
+const test = require('tape')
+const Omega = require('omega')
+
+const ImmutableStore = require('..')
+
+test('simple exchange', async t => {
+  const o1 = Omega.createProtocolStream(true)
+  const o2 = Omega.createProtocolStream(false)
+  o1.pipe(o2).pipe(o1)
+
+  const store1 = new ImmutableStore()
+  const store2 = new ImmutableStore()
+
+  store1.register(o1)
+  store2.register(o2)
+
+  const hash = await store1.put(Buffer.from('hello world'))
+
+  const val = await store2.get(hash)
+  t.same(val, Buffer.from('hello world'))
+
+  t.end()
+})
+
+test('second get should not send requests', async t => {
+  const o1 = Omega.createProtocolStream(true)
+  const o2 = Omega.createProtocolStream(false)
+  o1.pipe(o2).pipe(o1)
+
+  let s1rx = 0
+  let s2rx = 0
+  const store1 = new ImmutableStore({ debug: { onmessage: () => s1rx++ } })
+  const store2 = new ImmutableStore({ debug: { onmessage: () => s2rx++ } })
+
+  store1.register(o1)
+  store2.register(o2)
+
+  const hash = await store1.put(Buffer.from('hello world'))
+
+  {
+    const val = await store2.get(hash)
+    t.same(val, Buffer.from('hello world'))
+  }
+
+  {
+    // A second get should not hit the network.
+    const rxBeforeGet = s1rx
+    const val = await store2.get(hash)
+    t.same(val, Buffer.from('hello world'))
+    t.true(s1rx > 0)
+    t.same(s1rx, rxBeforeGet)
+  }
+
+  t.end()
+})
+
+test('values split between peers', async t => {
+  const o1 = Omega.createProtocolStream(true)
+  const o2 = Omega.createProtocolStream(false)
+  const o3 = Omega.createProtocolStream(true)
+  const o4 = Omega.createProtocolStream(false)
+  o1.pipe(o2).pipe(o1)
+  o3.pipe(o4).pipe(o3)
+
+  const store1 = new ImmutableStore()
+  const store2 = new ImmutableStore()
+  const store3 = new ImmutableStore()
+
+  /**
+   *         |----> store2 (hello)
+   *  store1
+   *         |----> store3 (world)
+   *
+   * */
+  store1.register(o1)
+  store1.register(o3)
+  store2.register(o2)
+  store3.register(o4)
+
+  const h1 = await store2.put(Buffer.from('hello'))
+  const h2 = await store3.put(Buffer.from('world'))
+
+  t.same(await store1.get(h1), Buffer.from('hello'))
+  t.same(await store1.get(h2), Buffer.from('world'))
+
+  t.end()
+})
+
+test('empty response scenarios', async t => {
+  const o1 = Omega.createProtocolStream(true)
+  const o2 = Omega.createProtocolStream(false)
+  o1.pipe(o2).pipe(o1)
+
+  let received = 0
+  const store1 = new ImmutableStore({
+    timeout: 50,
+    debug: {
+      onmessage (msg, from) {
+        t.true(msg.response && msg.response.discoveryKey)
+        t.false(msg.response.hash)
+        t.false(msg.response.value)
+        received++
+      }
+    }
+  })
+  const store2 = new ImmutableStore()
+
+  store1.register(o1)
+  store2.register(o2)
+
+  const h1 = await store2.put(Buffer.from('hello world'))
+
+  // Remote does not have this value -- empty response
+  await store1.get(randomBytes(32))
+
+  // Remote has the value, but the capability check should fail.
+  store1._send([...store1._exts.values()][0], {
+    request: {
+      discoveryKey: deriveHash(h1),
+      capability: randomBytes(32)
+    }
+  })
+
+  await new Promise(resolve => setImmediate(resolve))
+  t.same(received, 2)
+
+  t.end()
+})
+
+function randomBytes (len) {
+  const buf = Buffer.allocUnsafe(len)
+  sodium.randombytes_buf(buf)
+  return buf
+}
+
+function deriveHash (buf) {
+  const digest = Buffer.allocUnsafe(32)
+  sodium.crypto_generichash(digest, buf)
+  return digest
+}
