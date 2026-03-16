86428ee6e817e698b649200d257bc9d78da47c7c
Dominic Cassidy <dominic@cassidy.casa>
2026-03-16 15:47:35 +0200

    tests

--- test/all.js
+++ test/all.js
@@ -2,16 +2,18 @@
 
 runTests()
 
-async function runTests() {
+async function runTests () {
   const test = (await import('brittle')).default
 
   test.pause()
 
   await import('./basic.js')
-  await import('./concurrent-writes.js')
-  await import('./perf.js')
-  await import('./optimistic-race.js')
   await import('./batch.js')
+  await import('./concurrent-writes.js')
+  await import('./links.js')
+  await import('./optimistic-race.js')
+  await import('./perf.js')
+  await import('./updates.js')
   await import('./view-consistency.js')
   await import('./writer-management.js')
 
---
+++ test/updates.js
@@ -0,0 +1,23 @@
+const test = require('brittle')
+
+const { create, encode } = require('./helpers')
+
+test('updates - simple', async (t) => {
+  const auto = await create(t, { update })
+  t.plan(4)
+
+  await auto.append(encode({ hello: 'world' }))
+
+  t.is(auto.view.core.length, 1)
+
+  await auto.append(encode({ hello: 'world2' }))
+
+  t.is(auto.view.core.length, 2)
+
+  async function update(view, changes) {
+    const update = changes.get('view')
+
+    // always appending one
+    t.is(update.to - update.from, 1)
+  }
+})
