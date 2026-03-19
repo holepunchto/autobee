98a3f4a7e82481098f9589865c50bdd879b60c4e
Dominic Cassidy <dominic@cassidy.casa>
2026-03-19 00:15:39 +0000

    Add autobee-wakeup module

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ScopeLock = require('scope-lock')
 const Hyperbee = require('hyperbee2')
 const ID = require('hypercore-id-encoding')
 const { AutobeeEncryption, WriterEncryption } = require('autobee-encryption')
+const AutobeeWakeup = require('autobee-wakeup')
 const crypto = require('hypercore-crypto')
 const c = require('compact-encoding')
 const asserts = require('./lib/asserts.js')
@@ -65,6 +66,8 @@ module.exports = class Autobee extends ReadyResource {
     this._needsUpdate = false
     this._host = new ApplyCalls(this)
 
+    this._wakeup = new AutobeeWakeup(this, handlers)
+
     this.ready().catch(noop)
   }
 
@@ -87,6 +90,9 @@ module.exports = class Autobee extends ReadyResource {
     this._bootingAll.catch(noop)
 
     await this.bee.ready()
+
+    this._wakeup.recouple()
+    this._wakeup.setCapability(this.key, this.discoveryKey)
   }
 
   async _close() {
@@ -104,7 +110,9 @@ module.exports = class Autobee extends ReadyResource {
   }
 
   replicate(...args) {
-    return this.store.replicate(...args)
+    const stream = this.store.replicate(...args)
+    this._wakeup.addStream(stream)
+    return stream
   }
 
   async flush() {
---
+++ test/wakeup.js
@@ -0,0 +1,31 @@
+const test = require('brittle')
+const { create, replicateAndSync, encode, same } = require('./helpers')
+
+test('basic - replication', async function (t) {
+  const auto1 = await create(t)
+  const auto2 = await create(t, auto1.key)
+
+  await auto1.append(encode({ hello: 'world' }))
+  await auto1.append(encode({ addWriter: auto2.local.id }))
+
+  await replicateAndSync(auto1, auto2)
+
+  await auto2.append(encode({ hello: 'world' }))
+
+  await replicateAndSync(auto1, auto2)
+
+  t.ok(auto1._wakeup._coupler)
+  t.ok(auto1._wakeup._session)
+  t.ok(auto2._wakeup._coupler)
+  t.ok(auto2._wakeup._session)
+
+  await replicateAndSync(auto1, auto2)
+
+  t.is(auto1._wakeup._coupler.coupled.size, 2)
+  t.is(auto2._wakeup._coupler.coupled.size, 2)
+
+  t.comment('waiting')
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  t.ok(await same(auto1, auto2))
+})
