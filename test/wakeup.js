7e394e277d3be2146ffe4ec8ad06cba7f533646d
Sean Zellmer <sean@lejeunerenard.com>
2026-05-27 06:52:17 -0500

    Convert wakeup from store to use `notifyGroup()` API (#63)
    * Convert wakeup from store to use `notifyGroup()` API
    
    Added a test to showcase a scenario where the wakeup bumps the autobee.
    
    * chore: re-work to use updated notifyGroup api
    
    * chore: remove unused await
    
    * test: fix test flake
    
    * chore!: Bump hypercore and corestore
    
    ---------
    
    Co-authored-by: Dominic Cassidy <dominic@cassidy.casa>

--- test/wakeup.js
+++ test/wakeup.js
@@ -24,9 +24,13 @@ test('wakeup - replication', async function (t) {
 })
 
 test('wakeup - onwakeup', async function (t) {
+  t.plan(5)
+
+  const wakeups = []
+
   const auto1 = await create(t)
   const auto2 = await create(t, auto1.key)
-  const auto3 = await create(t, auto1.key, { onwakeup: createOnWakeup('auto3') })
+  const auto3 = await create(t, auto1.key, { onwakeup: createOnWakeup() })
 
   await auto1.append(encode({ hello: 'world' }))
   await auto1.append(encode({ addWriter: auto2.local.id, weight: 1 }))
@@ -42,8 +46,8 @@ test('wakeup - onwakeup', async function (t) {
 
   const expected = auto1.system.bee.head()
   const moved = new Promise((resolve) => {
-    auto3.on('move-to', (to) => {
-      t.alike(to, expected)
+    auto3.once('move-to', (to) => {
+      t.alike(to, expected, 'moved')
       resolve()
     })
   })
@@ -59,15 +63,83 @@ test('wakeup - onwakeup', async function (t) {
   t.teardown(replicate(auto1, auto3))
   t.ok(await same(auto2, auto3))
 
-  function createOnWakeup(name) {
+  // All up to date
+  {
+    const view = auto3._workingBee
+    const entry = await view.get(b4a.from('latest'))
+    const data = decode(entry.value)
+
+    t.alike(data, { hello: 'from auto2' })
+  }
+
+  t.ok(wakeups.length > 0, 'wokeup')
+  t.alike(wakeups[0], { hello: 'from auto2' })
+
+  function createOnWakeup() {
     return async function (view) {
       const entry = await view.get(b4a.from('latest'))
       const data = decode(entry.value)
 
-      t.is(name, 'auto3')
-      t.alike(data, { hello: 'from auto2' })
+      if (data.hello !== 'from auto2') return
+
+      wakeups.push(data)
 
       return { key: auto1.local.key, length: auto1.local.length }
     }
+  }
+})
+
+test('wakeup - onwakeup via store', async function (t) {
+  const auto1 = await create(t)
+  const dir = await t.tmp()
+  let auto2 = await create(t, auto1.key, { storage: dir })
+  const auto3 = await create(t, auto1.key)
+  const a2c3 = auto2.store.get(auto3.local.key)
+
+  await auto1.append(encode({ hello: 'world' }))
+  await auto1.append(encode({ addWriter: auto3.local.id, weight: 1 }))
+
+  await replicateAndSync(auto1, auto2, auto3)
+
+  await a2c3.ready()
+
+  t.is(auto3.local.length, a2c3.length, 'auto2 got auto1 writer')
+  await a2c3.close()
+  t.ok(
+    auto2.store.cores.map.has(auto3.local.discoveryKey.toString('hex')),
+    'auto3 core loaded in auto2 before reopening'
+  )
+  await auto2.close()
+
+  t.comment('reopen auto2 to clear corestore cores')
+  auto2 = await create(t, auto1.key, { storage: dir })
+
+  t.absent(
+    auto2.store.cores.map.has(auto3.local.key.toString('hex')),
+    'auto3 core not loaded in auto2 store after reopen'
+  )
+
+  // Only replicate stores so no wakeup protomux messages
+  t.teardown(replicate(auto1.store, auto2.store))
+
+  await auto2.writers.refresh()
+
+  t.absent(
+    auto2.store.cores.map.has(auto3.local.key.toString('hex')),
+    'auto3 core not loaded in auto2 store after reopen'
+  )
+  t.absent(
+    auto2.writers.has(auto3.local.key.toString('hex')),
+    'auto3 core not loaded as active writer in auto2 after reopen'
+  )
+
+  await auto3.append(encode({ foo: 'bar' }))
+  await replicateAndSync(auto1, auto3)
+
+  t.ok(await same(auto2, auto3), 'autobees match')
+  {
+    const entry = await auto2.view.get(b4a.from('latest'))
+    const data = decode(entry.value)
+    t.alike(data, { foo: 'bar' }, 'auto2 has updates from auto3')
   }
 })
