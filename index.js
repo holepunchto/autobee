29a0562d7867ea68625113a64c7f0d7dfaf3750c
Sean Zellmer <sean@lejeunerenard.com>
2026-05-27 10:09:34 -0500

    Remove check for `target === -1`
    Not possible to hit because its defined as either a `>= 0` value or
    `core.length` which is `>= 0`.

--- index.js
+++ index.js
@@ -410,7 +410,6 @@ module.exports = class Autobee extends ReadyResource {
     await core.ready()
 
     const target = length >= 0 ? length : core.length
-    if (target === -1) return null
 
     const buf = await core.get(target - 1)
     await core.close()
