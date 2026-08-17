ca3b715e46eb2e493dfe727e998296b798cfc626
Christophe Diederichs <chm-diederichs@protonmail.com>
2026-08-17 16:26:07 +0100

    fix flushes stamp on local node

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ module.exports = class Autobee extends ReadyResource {
 
     this._localSystemStart = 0
     this._localSystemLength = 0
+    this._localFlushes = 0
     this._localViewStart = 0
     this._localViewLength = 0
 
@@ -169,6 +170,7 @@ module.exports = class Autobee extends ReadyResource {
 
     this._localSystemStart = this.system.bee.context.local.length
     this._localViewStart = this._workingBee.context.local.length
+    this._localFlushes = this.system.flushes
 
     this.bumpSoon()
   }
@@ -984,6 +986,7 @@ module.exports = class Autobee extends ReadyResource {
     if (local) {
       this._localSystemLength = this.system.bee.context.local.length - this._localSystemStart
       this._localViewLength = this._workingBee.context.local.length - this._localViewStart
+      this._localFlushes = this.system.flushes
     }
 
     this.writers.attest(witnessed)
@@ -1092,7 +1095,7 @@ module.exports = class Autobee extends ReadyResource {
 
   async _flushLocal() {
     await this.writers.flushLocal({
-      flushes: this.system.flushes,
+      flushes: this._localFlushes,
       system: {
         key: this.system.bee.context.local.key,
         start: this._localSystemStart,
@@ -1107,6 +1110,7 @@ module.exports = class Autobee extends ReadyResource {
 
     this._localSystemStart = this.system.bee.context.local.length
     this._localSystemLength = 0
+    this._localFlushes = this.system.flushes
     this._localViewStart = this._workingBee.context.local.length
     this._localViewLength = 0
   }
@@ -1123,13 +1127,15 @@ module.exports = class Autobee extends ReadyResource {
       return false
     }
 
-    const moved = await this._moveTo(batchToHead(verified.op.views.system), {
+    const tip = {
       system: batchToHead(oplog.op.views.system),
       verified: {
         op: trusted || head,
         flushes: verified.op.views.flushes
       }
-    })
+    }
+
+    const moved = await this._moveTo(batchToHead(verified.op.views.system), tip)
 
     if (moved && wait) return this.reboot.promise
 
