6c12cfe19135651b2476623f2274ed4d09caff60
Christophe Diederichs <chm-diederichs@protonmail.com>
2026-08-19 16:37:48 +0100

    kill active requests in ff

--- index.js
+++ index.js
@@ -250,6 +250,8 @@ module.exports = class Autobee extends ReadyResource {
         session.clearRequests(session.activeRequests, closingError)
       }
     }
+
+    if (this.fastForwarding) this.fastForwarding.clearRequests(closingError)
   }
 
   async changesFrom({ system } = {}) {
