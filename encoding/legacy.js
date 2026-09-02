801c5abc43319cb44f5f9523e0de3f8405b43199
Mathias Buus <mathiasbuus@gmail.com>
2026-09-02 13:15:25 +0200

    Ensure mapped shape (#209)
    * housekeeping
    
    * ensure mapped shape

--- encoding/legacy.js
+++ encoding/legacy.js
@@ -280,8 +280,8 @@ const SystemWriterV0 = {
 
     return {
       version: 0,
-      isIndexer: flags & 1,
-      isRemoved: flags & 2,
+      isIndexer: (flags & 1) !== 0,
+      isRemoved: (flags & 2) !== 0,
       length: c.uint.decode(state)
     }
   }
@@ -303,8 +303,14 @@ function memberLegacyMap(m) {
   return {
     version: 0,
     isRemoved: m.isRemoved,
+    isOplog: false,
     weight: m.isIndexer ? 2 : 1,
-    length: m.length
+    maxWeight: m.isIndexer ? 2 : 1,
+    length: m.length,
+    clock: 0,
+    isGenesis: false,
+    isAnchor: false,
+    timestamp: 0
   }
 }
 
