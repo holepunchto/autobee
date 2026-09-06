7ba0a26b546185fddf0cf882d0ab74ee09be6687
Mathias Buus <mathiasbuus@gmail.com>
2026-09-06 20:37:32 +0200

    fix skip

--- test/migration.js
+++ test/migration.js
@@ -10,7 +10,7 @@ const os = IS_BARE ? null : require('os')
 const Autobee = require('../index.js')
 const { replicate, sync } = require('./helpers')
 
-const skip = IS_BARE || ['linux', 'darwin'].includes(os.platform())
+const skip = IS_BARE || !['linux', 'darwin'].includes(os.platform())
 
 const path = skip ? null : require('path')
 const fs = skip ? null : require('fs/promises')
