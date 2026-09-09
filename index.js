58090915a122387de9f8db805e1d3b62b1c24ce3
Mathias Buus <mathiasbuus@gmail.com>
2026-09-09 22:55:50 +0200

    switch default for conservative

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ module.exports = class Autobee extends ReadyResource {
     this.bootFrom = (fastForward && fastForward.boot) || null
 
     // conservative (default on): only fast-forward onto a head someone can serve whole
-    this._conservativeFF = !fastForward || fastForward.conservative !== false
+    this._conservativeFF = !fastForward || !!fastForward.conservative
 
     this.trusted = new TrustedPeers(this, handlers)
 
