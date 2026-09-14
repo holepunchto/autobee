9e8c05f96ec2910176dd7be0bedd9ec02afa0412
Mathias Buus <mathiasbuus@gmail.com>
2026-09-14 12:38:52 +0200

    also add anchor

--- index.js
+++ index.js
@@ -1039,7 +1039,7 @@ module.exports = class Autobee extends ReadyResource {
 
     const anchor = { key: core.key, length: core.length }
 
-    this.emit('anchor', core)
+    this.emit('anchor', anchor, core)
 
     await core.close()
 
