d06ce3e5faf998cf2875a9c4742c1901e2441849
Mathias Buus <mathiasbuus@gmail.com>
2026-09-16 13:03:50 +0200

    Revert (#312)
    * Revert "fix appending checks (#307)"
    
    This reverts commit 409c61f70f1a8cc08875d70532de90a7874d00c2.
    
    * Revert "Fix versioned structs with legacy decoders (#309)"
    
    This reverts commit 0dcd3ba5f22dceaeacff1e4be026aaa8b8135e91.

--- index.js
+++ index.js
@@ -127,6 +127,7 @@ module.exports = class Autobee extends ReadyResource {
     this._localViewStart = 0
     this._localViewLength = 0
 
+    this._appending = []
     this._draining = null
     this._updating = null
 
@@ -176,10 +177,6 @@ module.exports = class Autobee extends ReadyResource {
 
   get isIndexer() {
     return this.writers ? this.writers.localWriter.isIndexer : false
-  }
-
-  get appending() {
-    return this.writers ? this.writers.localWriter.appending : false
   }
 
   get writable() {
